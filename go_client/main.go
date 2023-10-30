7f6de666580170e077f5b79a7f90bf8b9a8cf29d
Xiang Chen <san@fluidndynamic.com>
2023-10-30 07:53:14 -0400

    added go code to do embedding

---
+++ go_client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	huggingface "github.com/hupe1980/go-huggingface"
+)
+
+func main() {
+	ic := huggingface.NewInferenceClient(os.Getenv("HUGGINGFACEHUB_API_TOKEN"))
+
+	res1, err1 := ic.FeatureExtraction(context.Background(), &huggingface.FeatureExtractionRequest{
+		Inputs: []string{"Hello World"},
+		Options: huggingface.Options{
+			WaitForModel: huggingface.PTR(true),
+		},
+	})
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	fmt.Println("FeatureExtraction:")
+	fmt.Println(res1[0])
+	fmt.Println()
+
+	res2, err2 := ic.FeatureExtractionWithAutomaticReduction(context.Background(), &huggingface.FeatureExtractionRequest{
+		Inputs: []string{"Hello World"},
+		Model:  "sentence-transformers/all-mpnet-base-v2",
+		// Model:  "intfloat/e5-small-v2",
+		Options: huggingface.Options{
+			WaitForModel: huggingface.PTR(true),
+		},
+	})
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("FeatureExtractionWithAutomaticReduction:")
+	fmt.Println(res2[0])
+	fmt.Println()
+}
