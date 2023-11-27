8ddf3faacfba259c4dd4ba7ddc3749f4b8bedcd2
Xiang Chen <san@fluidndynamic.com>
2023-11-26 20:20:23 -0500

    Final project commit for the web app

--- go_client/main.go
+++ go_client/main.go
@@ -7,8 +7,6 @@ import (
 	"os"
 	"net/http"
 	"time"
-
-	huggingface "github.com/hupe1980/go-huggingface"
 )
 
 
@@ -38,38 +36,4 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	server.ListenAndServe()
-}
-
-func hf_main() {
-	ic := huggingface.NewInferenceClient(os.Getenv("HUGGINGFACEHUB_API_TOKEN"))
-
-	res1, err1 := ic.FeatureExtraction(context.Background(), &huggingface.FeatureExtractionRequest{
-		Inputs: []string{"Hello World"},
-		Options: huggingface.Options{
-			WaitForModel: huggingface.PTR(true),
-		},
-	})
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-
-	fmt.Println("FeatureExtraction:")
-	fmt.Println(res1[0])
-	fmt.Println()
-
-	res2, err2 := ic.FeatureExtractionWithAutomaticReduction(context.Background(), &huggingface.FeatureExtractionRequest{
-		Inputs: []string{"Hello World"},
-		Model:  "sentence-transformers/all-mpnet-base-v2",
-		// Model:  "intfloat/e5-small-v2",
-		Options: huggingface.Options{
-			WaitForModel: huggingface.PTR(true),
-		},
-	})
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-	fmt.Println("FeatureExtractionWithAutomaticReduction:")
-	fmt.Println(res2[0])
-	fmt.Println()
 }
